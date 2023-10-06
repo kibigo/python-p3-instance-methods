018a0afb5b4677289ea4304bc112c978735ddcbc
kibigo <jacksonkibigo99@gmail.com>
2023-10-06 22:38:35 +0300

    Completed Assignment

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+john = Person()
+john.talk()
+
+peter = Person()
+peter.walk()
